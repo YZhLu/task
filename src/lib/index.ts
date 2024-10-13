415bd6a20a9346e6cbb4e4d98ff3d3bc529ce923
YZhLu <luiz.zito@icloud.com>
2024-10-13 01:17:19 -0300

    add Alternative Type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,4 +2,5 @@
 export { default as TaskPage } from './Pages/Task/TaskPage.svelte';
 export { default as TaskEditPage } from './Pages/Task/TaskEditPage.svelte';
 export { default as TaskRegisterPage } from './Pages/Task/TaskRegisterPage.svelte';
+export type { Alternative } from './types/Alternative';
 export type { TaskRegisterStoreData } from './types/TaskRegisterStoreData';
