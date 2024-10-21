9082a20a544d97de5666318cdd2f29a006b7094a
YZhLu <luiz.zito@icloud.com>
2024-10-20 22:45:35 -0300

    Update +page.ts

--- src/routes/task/+page.ts
+++ src/routes/task/+page.ts
@@ -1,7 +1,7 @@
 import SERVER from '$lib/Pages/config';
 import type { PageLoad } from './$types';
 
-const tid = 'c89f81d5-197d-47cd-9033-5336b73a783d'; //af286099-894d-499b-902f-0dd24357cc23/` //e4c20e5b-00bd-4140-82a4-2d7c4af33f5f
+const tid = '3f4b9dfc-8246-4713-bc9d-edb895dcc00e'//'c89f81d5-197d-47cd-9033-5336b73a783d'; //af286099-894d-499b-902f-0dd24357cc23/` //e4c20e5b-00bd-4140-82a4-2d7c4af33f5f
 export const load: PageLoad = async ({ fetch }) => {
 	const fetchUnvalidatedTask = async () => {
 		const tasksRes = await fetch(SERVER + `/tasks/${tid}/`);
